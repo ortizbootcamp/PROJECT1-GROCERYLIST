c65d8d60c78c8687794c36c027d0b04c74b9c9d7
Natasha Ortiz <tashaaortiz@gmail.com>
2021-09-01 20:06:11 -0500

    added project information to code files

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+ 1) Dashboard & Login Page
+    -Username
+    -Password
+
+2) Navbar, Sidenav & Search
+    -List of current groceries (sidenav)
+    -Categories of groceries (Sidenav)
+    -Search (Navbar)
+     >Autocomplete
+     >User Input
+     >If user types in item we do not have, 'item not available' appears
+
+3) Recipe suggestion in Navbar
+    -based on what items you have in your list
+
+4)Email/Phone Number to add family member onto list
+    -When they add an item, their name/image is attached to their items
+    -Main user will be able to delete/edit their items 
+*/
