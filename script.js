56ca9eb7540d8a44c8505b612e74e6513cce740e
Matt ✔️ <matsavaqe26@gmail.com>
2021-09-08 20:02:40 -0500

    Added search box

--- script.js
+++ script.js
@@ -17,7 +17,6 @@
 4)Email/Phone Number to add family member onto list
     -When they add an item, their name/image is attached to their items
     -Main user will be able to delete/edit their items 
-<<<<<<< HEAD
 */
 
 $(document).ready(function(){
@@ -26,6 +25,4 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.modal').modal();
 });
-=======
-*/
->>>>>>> c65d8d60c78c8687794c36c027d0b04c74b9c9d7
+
