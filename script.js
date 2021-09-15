3de8cb5060bb7e8bc184a852326cc9c3d9f82fc0
brooke-delgado <83773208+brooke-delgado@users.noreply.github.com>
2021-09-14 20:50:45 -0500

    linked drop down pictures to recipe api, cleaned up code, local storage, search button to work on button click or when enter is pressed

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
- 1) Dashboard & Login Page
-    -Username
-    -Password
-
-
-2) Navbar, Sidenav & Search
-    -List of current groceries (sidenav)
-    -Categories of groceries (Sidenav)
-    -Search (Navbar)
-     >Autocomplete
-     >User Input
-     >If user types in item we do not have, 'item not available' appears
-
-3) Recipe suggestion in Navbar
-    -based on what items you have in your list
-    section id- favrecipes */
 //recipe API code
 const settings = {
   async: true,
@@ -27,17 +10,42 @@ const settings = {
   },
 };
 
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-
 //button click to go to recipe puppy
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+function myFunction() {
+  document.getElementById("recipe-button").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches(".dropdown-button")) {
+    var myDropdown = document.getElementById("recipe-button");
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
+    }
+  }
+};
+
+$(document).ready(function () {
+  $("input.autocomplete").autocomplete({
+    data: {
+      Apples: null,
+      Bananas: null,
+      Oranges: null,
+    },
+  });
+});
 
 var button = document.getElementById("recipe-button");
 button.addEventListener("click", function () {
   //add funtion to grab users choices//
+  var randomNum = Math.floor(Math.random() * 1483) + 1;
   fetch(
-    "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes",
+    "https://tasty.p.rapidapi.com/recipes/list?from=" +
+      randomNum +
+      "&size=10&tags=under_30_minutes", // from = what index to start at | size = recipes returned max: 40 |
     settings
   )
     .then(function (result) {
@@ -45,40 +53,62 @@ button.addEventListener("click", function () {
     })
     .then(function (result) {
       console.log(result);
+      var randomPicture = [];
+      for (let i = 0; i < 10; i++) {
+        randomPicture.push(result.results[i]);
+        console.log(result.results[i].slug); // "tasty.co/recipes/" + slug <= needs to go in your anchor tag
+      }
+      console.log(randomPicture);
       //all logic for adding recipes to screen
-      for (let i = 0; i < result.results.length; i++) {
+      var container = document.createElement("div");
+      container.id = "recipe";
+      for (let i = 0; i < 10; i++) {
         var recipe = result.results[i];
-        var recipeInfo = document.getElementById("recipe-info");
+        var recipeLink = document.createElement("a");
+        recipeLink.setAttribute(
+          "href",
+          "https://tasty.co/recipe/" + result.results[i].slug
+        );
+        // var recipeInfo = document.getElementById("recipe-info");
         var recipeImage = document.createElement("img");
         recipeImage.setAttribute("src", recipe.thumbnail_url);
-        recipeInfo.appendChild(recipeImage);
+        //  recipeInfo.appendChild(recipeImage);
+        recipeLink.appendChild(recipeImage);
+        container.appendChild(recipeLink);
+        recipeImage.style.width = "250px";
+        recipeImage.style.height = "auto";
+        recipeImage.style.display = "flex";
       }
+      document.body.appendChild(container);
     })
     .catch(function (err) {
       console.error(err);
     });
 });
 
-//DOM
+/*API*/
 
-/*4)Email/Phone Number to add family member onto list
-    -When they add an item, their name/image is attached to their items
+var searchTerm = $("#search-input").val();
+var spoonRequest = "?apiKey=21f5dc0d9fd041aca40b7098e690844d";
+var genSearch = "https://api.spoonacular.com/food/products/search";
+//local storage for current grocery list
 
-    -Main user will be able to delete/edit their items */
+//grocery items
+var item = "";
 
-$(document).ready(function () {
-  $(".sidenav").sidenav();
-});
-$(document).ready(function () {
-  $(".modal").modal();
-});
+var groceryList = function () {
+  var groceryList = [];
 
-    -Main user will be able to delete/edit their items 
-*/
+  function item(name, quantity) {
+    this.name = name;
+    this.quantity = quantity;
+  }
+  function save() {
+    localStorage.setItem("groceryList", JSON.stringify(groceryList));
+  }
 
-$(document).ready(function(){
-    $('.sidenav').sidenav()
-});
-$(document).ready(function(){
-    $('.modal').modal();
-});
+  function load() {
+    groceryList = JSON.parse(localStorage.getItem(groceryList)) || [];
+  }
+  load;
+};
